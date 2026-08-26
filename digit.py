e4d305e6d31689012405e22e8eb53883e6c8e76a
Taahaa Mussagibai <tmussagibai@gmail.com>
2026-08-26 20:52:28 +0100

    Started Model and Digit class development + added .gitignore

--- digit.py
+++ digit.py
@@ -1,2 +1,53 @@
-class Digit():
-    pass
+import sys
+import numpy as np
+import struct
+
+
+class Digits():
+    def __init__(self, digits_file, digit_labels_file):
+        self._read_digits(digits_file)
+        self._read_digit_labels(digit_labels_file)
+
+    def _read_digits(self, digits_file):
+        with open(digits_file, "rb") as f:
+            header = f.read(16)
+
+            # 4 big-endian uint32
+            magic, count, rows, cols = struct.unpack(">IIII", header)
+
+        try:
+            assert(magic==2051)
+        except:
+            print(f"{digits_file} is an invalid digits file. Use the MNIST dataset", file=sys.stderr)
+            quit()
+
+        self.digits_list = [np.empty(shape=(rows * cols,), dtype=np.float64) for _ in range(count)]
+
+
+    def _read_digit_labels(self, digit_labels_file):
+        with open(digit_labels_file, "rb") as f:
+            header = f.read(8)
+
+            # 2 big-endian uint32
+            magic, label_count = struct.unpack(">II", header)
+
+        try:
+            assert(magic==2051)
+        except:
+            print(f"{digit_labels_file} is an invalid labels file. Use the MNIST dataset", file=sys.stderr)
+            quit()
+
+        self.digit_labels_list = [
+            np.empty(
+                shape=(10,),
+                dtype=np.float64
+            ) for _ in range(label_count)
+        ]
+
+
+    def get_digits_list(self):
+        pass
+
+
+    def get_digit_labels_list(self):
+        pass
--- model.py
+++ model.py
@@ -1,10 +1,69 @@
-from digit import Digit
+import sys
+import numpy as np
 
 
 class Model():
-    def __init__(self, model_file):
+    # indices for layer indexing
+    WGHT_MATRIX = 0
+    BIAS_VEC = 1
+    # TODO: Store and load model from file
+    # neuron_count_list describes the number of neurons in each layer from start to end
+    def __init__(self, input_size, output_size, neuron_count_list):
+        try:
+            assert(neuron_count_list[0] == input_size)
+            assert(neuron_count_list[-1] == output_size)
+        except:
+            print("neuron_count_list[0] must be the same as input_size", file=sys.stderr)
+            print("neuron_count_list[-1] must be the same as output_size", file=sys.stderr)
+            quit()
+
+        self.layers = []
+        self.layer_count = len(neuron_count_list)
+
+        for i in range(1, self.layer_count):
+            lyr_input_size = neuron_count_list[i-1]
+            lyr_output_size = neuron_count_list[i]
+
+            self.layers.append([
+                np.random.default_rng().normal(  # weight matrix
+                    0.0,
+                    np.sqrt(2.0 / lyr_input_size),
+                    size=(lyr_output_size, lyr_input_size),
+                ),
+                np.zeros(                        # bias vector
+                    lyr_output_size,
+                    dtype=np.float64
+                )
+            ])
+
+
+    def _get_cost(self, output, expected_output):  # vectors of same size
         pass
 
 
-    def train(self, digit):
+    def _backpropagate(self):
+        pass
+
+
+    def run_model(self, input):
+        for lyr in self.layers:
+            input = lyr[Model.WGHT_MATRIX] @ input + lyr[Model.BIAS_VEC]
+
+        """
+        return self._run_model(self.layer_count - 1, input)
+        """
+        return input
+
+
+    # recursive implementation (maybe its better without Python objects getting in the way?)
+    def _run_model(self, lyr_idx, input):
+        if lyr_idx < 0: return input
+
+        return self._run_model(
+            lyr_idx - 1,
+            self.layers[lyr_idx][Model.WGHT_MATRIX] @ input + self.layers[lyr_idx][Model.BIAS_VEC],
+        )
+
+
+    def save_model(self):  # need to define a model file structure
         pass
