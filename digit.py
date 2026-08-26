062cdda6d4fea0406c0a6f13ee9161002e0f5115
Taahaa Mussagibai <tmussagibai@gmail.com>
2026-08-26 22:17:57 +0100

    Created initial implementation for digit loading from file

--- digit.py
+++ digit.py
@@ -8,6 +8,16 @@ class Digits():
         self._read_digits(digits_file)
         self._read_digit_labels(digit_labels_file)
 
+        try:
+            assert(self._label_count == self._count == self._rows * self._cols)
+        except Exception as e:
+            print(e, file=sys.stderr)
+            print(
+                f"Using this Digits object ({self}) is not advised.\n" +
+                "Behaviour will be undefined.\n",
+                file=sys.stderr
+            )
+
     def _read_digits(self, digits_file):
         with open(digits_file, "rb") as f:
             header = f.read(16)
@@ -15,13 +25,23 @@ class Digits():
             # 4 big-endian uint32
             magic, count, rows, cols = struct.unpack(">IIII", header)
 
-        try:
-            assert(magic==2051)
-        except:
-            print(f"{digits_file} is an invalid digits file. Use the MNIST dataset", file=sys.stderr)
-            quit()
+            try:
+                assert(magic == 2051)
+            except:
+                print(f"{digits_file} is an invalid digits file. Use the MNIST dataset", file=sys.stderr)
+                return
 
-        self.digits_list = [np.empty(shape=(rows * cols,), dtype=np.float64) for _ in range(count)]
+            self._count = count
+            self._rows = rows
+            self._cols = cols
+
+            self._digits_list = [0 for _ in range(count)]  # everything is a pointer type, right?
+            for i in range(count):
+                self._digits_list[i] = np.fromfile(
+                    f,
+                    dtype=np.uint8,
+                    count=(rows * cols)
+                ).astype(np.float64) / float(2**8)
 
 
     def _read_digit_labels(self, digit_labels_file):
@@ -31,23 +51,27 @@ class Digits():
             # 2 big-endian uint32
             magic, label_count = struct.unpack(">II", header)
 
-        try:
-            assert(magic==2051)
-        except:
-            print(f"{digit_labels_file} is an invalid labels file. Use the MNIST dataset", file=sys.stderr)
-            quit()
+            try:
+                assert(magic==2051)
+            except:
+                print(f"{digit_labels_file} is an invalid labels file. Use the MNIST dataset", file=sys.stderr)
+                return
 
-        self.digit_labels_list = [
-            np.empty(
-                shape=(10,),
-                dtype=np.float64
-            ) for _ in range(label_count)
-        ]
+            self._label_count = label_count
+
+            self._digit_labels_list = list(
+                np.zeros(shape=(10,), dtype=np.float64)
+                for _ in range(label_count)
+            )
+            for i in range(label_count):
+                header = f.read(1)
+                self._digit_labels_list[i][struct.unpack("B", header)] = 1.0
+                # label '0' will be the first in the array at idx 0
 
 
     def get_digits_list(self):
-        pass
+        return self._digits_list
 
 
     def get_digit_labels_list(self):
-        pass
+        return self._digit_labels_list
